6d315e51f8acd9554cee6a93d56385c29531af22
Michał Kotowski <m.jan.kotowski@gmail.com>
2023-05-28 20:12:56 +0200

    kolejnosc

--- chessendgames_api/routes/routes.js
+++ chessendgames_api/routes/routes.js
@@ -4,6 +4,22 @@ import DBConnection from "../services/dbConnection.js"
 
 const router = express.Router();
 
+router.post("/positions", async function(req, res) {
+    try {
+        var symbol = req.body["symbol"];
+    } catch (error) {
+        return res.status(400).json({status: 400, message: "Cannot find arguments"});
+    }
+
+    if ((symbol == "") || (symbol == undefined)) {
+        return res.status(400).json({status: 400, message: "SYMBOL must be present"});
+    }
+    
+    var conn = new DBConnection();
+    var response = await conn.getPositions(symbol);
+    res.json(response);
+});
+
 router.post("/analysePosition", async function(req, res) {
     try {
         var fen = req.body["fen"];
@@ -18,22 +34,6 @@ router.post("/analysePosition", async function(req, res) {
 router.post("/endgameTypes", async function(req, res) {
     var conn = new DBConnection();
     var response = await conn.getEndgameTypes();
-    res.json(response);
-});
-
-router.post("/positions", async function(req, res) {
-    try {
-        var symbol = req.body["symbol"];
-    } catch (error) {
-        return res.status(400).json({status: 400, message: "Cannot find arguments"});
-    }
-
-    if ((symbol == "") || (symbol == undefined)) {
-        return res.status(400).json({status: 400, message: "SYMBOL must be present"});
-    }
-    
-    var conn = new DBConnection();
-    var response = await conn.getPositions(symbol);
     res.json(response);
 });
 
